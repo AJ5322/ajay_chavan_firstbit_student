19ef4c1972e3f628e663c4a68429b07196ae1e54
AJ5322 <ajaysantoshchavan7@gmail.com>
2025-06-29 23:58:06 +0530

    Add files via upload

---
+++ C programming/class tests/Test 3/test3_q1.c
@@ -0,0 +1,25 @@
+void main()
+{
+	int start=10,end=25,no,i;
+	//printf("enter a number");
+	//scanf("%d",&no);
+	
+	for(i=0;i<no;i++)
+	{
+		if(no%2==0)
+		{
+			scanf("%d",&no);
+				
+		}
+			printf("even");
+	}
+	for(i=0;i<no;i++)
+	{
+		if(no%2!=0)
+		{
+			scan
+			f("%d",&no);
+		}
+		printf("no is odd");
+	}
+}
